ea78bb74a59ed867cd0b9c7037d9b23716e86af9
Rilwanu Isyaku <rilwanu@engineer.com>
2025-01-26 13:39:28 +0100

    Feat - static.json file

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VueDevTools(), tailwindcss()],
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
